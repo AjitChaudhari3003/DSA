6d40ce659c3a837d472b08d084e84e2a58d8bd00
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2026-02-21 13:44:23 +0530

    Create D52.java

---
+++ D52.java
@@ -0,0 +1,38 @@
+// 762. Prime Number of Set Bits in Binary Representation
+// Given two integers left and right, return the count of numbers in the inclusive range [left, right] having a prime number of set bits in their binary representation.
+// Recall that the number of set bits an integer has is the number of 1's present when written in binary.
+// For example, 21 written in binary is 10101, which has 3 set bits.
+// Example 1:
+// Input: left = 6, right = 10
+// Output: 4
+// Explanation:
+// 6  -> 110 (2 set bits, 2 is prime)
+// 7  -> 111 (3 set bits, 3 is prime)
+// 8  -> 1000 (1 set bit, 1 is not prime)
+// 9  -> 1001 (2 set bits, 2 is prime)
+// 10 -> 1010 (2 set bits, 2 is prime)
+// 4 numbers have a prime number of set bits
+
+
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int[] primes = {2,3,5,7,11,13,17,19};
+        java.util.Set<Integer> primeSet = new java.util.HashSet<>();
+        
+        for(int p : primes) {
+            primeSet.add(p);
+        }
+        
+        int ans = 0;
+        
+        for(int num = left; num <= right; num++) {
+            int setBits = Integer.bitCount(num);
+            
+            if(primeSet.contains(setBits)) {
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+}
