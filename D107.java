e4954f36a3950e8ffd438de180eb56d58a0da675
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2026-04-17 07:56:48 +0530

    D107.java

---
+++ D107.java
@@ -0,0 +1,54 @@
+3761. Minimum Absolute Distance Between Mirror Pairs
+Medium
+Topics
+premium lock icon
+Companies
+You are given an integer array nums.
+
+A mirror pair is a pair of indices (i, j) such that:
+
+0 <= i < j < nums.length, and
+reverse(nums[i]) == nums[j], where reverse(x) denotes the integer formed by reversing the digits of x. Leading zeros are omitted after reversing, for example reverse(120) = 21.
+Return the minimum absolute distance between the indices of any mirror pair. The absolute distance between indices i and j is abs(i - j).
+
+If no mirror pair exists, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [12,21,45,33,54]
+
+Output: 1
+
+Explanation:
+
+The mirror pairs are:
+
+(0, 1) since reverse(nums[0]) = reverse(12) = 21 = nums[1], giving an absolute distance abs(0 - 1) = 1.
+(2, 4) since reverse(nums[2]) = reverse(45) = 54 = nums[4], giving an absolute distance abs(2 - 4) = 2.
+The minimum absolute distance among all pairs is 1.
+
+
+
+
+class Solution {
+    public int minMirrorPairDistance(int[] nums) {
+        int res = 100000, i = 0;
+        HashMap<Integer, Integer> seen = new HashMap<>();
+
+        for (int n : nums) {
+            int r;
+            if (seen.containsKey(n))
+                res = Math.min(res, i - seen.get(n));
+
+            for (r = 0; n > 0; n /= 10)
+                r = r * 10 + (n % 10);
+
+            seen.put(r, i++);
+        }
+
+        return res == 100000 ? -1 : res;
+    }
+}
+  
