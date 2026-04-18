2f5a32374304c80685be9b62f076b2b2f5146c25
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2026-04-18 21:56:42 +0530

    Update D108.java

--- D108.java
+++ D108.java
@@ -1,28 +1,14 @@
-3783. Mirror Distance of an Integer
-Easy
-Topics
-premium lock icon
-Companies
-You are given an integer n.
-
-Define its mirror distance as: abs(n - reverse(n)) where reverse(n) is the integer formed by reversing the digits of n.
-
-Return an integer denoting the mirror distance of n.
-
-abs(x) denotes the absolute value of x.
-
- 
-
-Example 1:
-
-Input: n = 25
-
-Output: 27
-
-Explanation:
-
-reverse(25) = 52.
-Thus, the answer is abs(25 - 52) = 27.
+// 3783. Mirror Distance of an Integer
+// You are given an integer n.
+// Define its mirror distance as: abs(n - reverse(n)) where reverse(n) is the integer formed by reversing the digits of n.
+// Return an integer denoting the mirror distance of n.
+// abs(x) denotes the absolute value of x.
+// Example 1:
+// Input: n = 25
+// Output: 27
+// Explanation:
+// reverse(25) = 52.
+// Thus, the answer is abs(25 - 52) = 27.
 
 
 
