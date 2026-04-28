f1a357f787e3afa6caa4558d12ae256431f7a944
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2026-04-28 06:23:43 +0530

    Create D118.java

---
+++ D118.java
@@ -0,0 +1,38 @@
+// 2033. Minimum Operations to Make a Uni-Value Grid
+// You are given a 2D integer grid of size m x n and an integer x. In one operation, you can add x to or subtract x from any element in the grid.
+// A uni-value grid is a grid where all the elements of it are equal.
+// Return the minimum number of operations to make the grid uni-value. If it is not possible, return -1.
+// Example 1:
+// Input: grid = [[2,4],[6,8]], x = 2
+// Output: 4
+// Explanation: We can make every element equal to 4 by doing the following: 
+// - Add x to 2 once.
+// - Subtract x from 6 once.
+// - Subtract x from 8 twice.
+// A total of 4 operations were used.
+
+import java.util.*;
+
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int[] row : grid) {
+            for (int val : row) {
+                list.add(val);
+            }
+        }
+
+        Collections.sort(list);
+        int median = list.get(list.size() / 2);
+
+        int operations = 0;
+        for (int num : list) {
+            int diff = Math.abs(num - median);
+            if (diff % x != 0) return -1;
+            operations += diff / x;
+        }
+
+        return operations;
+    }
+}
