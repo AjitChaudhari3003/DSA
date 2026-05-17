4c70534486ceb7de2c964301240e863fab4e4af4
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2026-05-17 10:25:46 +0530

    Create D137.java

---
+++ D137.java
@@ -0,0 +1,39 @@
+// 1306. Jump Game III
+// Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
+// Notice that you can not jump outside of the array at any time.
+// Example 1:
+// Input: arr = [4,2,3,0,3,1,2], start = 5
+// Output: true
+// Explanation: 
+// All possible ways to reach at index 3 with value 0 are: 
+// index 5 -> index 4 -> index 1 -> index 3 
+// index 5 -> index 6 -> index 4 -> index 1 -> index 3
+
+
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new ArrayDeque<>();
+
+        q.offer(start);
+
+        while (!q.isEmpty()) {
+            int i = q.poll();
+
+            if (i < 0 || i >= n || visited[i])
+                continue;
+
+            if (arr[i] == 0)
+                return true;
+
+            visited[i] = true;
+
+            q.offer(i + arr[i]);
+            q.offer(i - arr[i]);
+        }
+
+        return false;
+    }
+}
