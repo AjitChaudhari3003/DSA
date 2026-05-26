4edad609f8ff6b7da102bd34f2596aedbfaacf93
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2026-05-26 17:13:43 +0530

    Create D146.java

--- D146.java
+++ D146.java
@@ -1,8 +1,4 @@
-3120. Count the Number of Special Characters I
-Easy
-Topics
-premium lock icon
-Companies
+3120. Count the Number of Special Character
 You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
 
 Return the number of special letters in word.
